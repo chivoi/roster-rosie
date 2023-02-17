45f56e69120f046871e2b0b481ee0840c6db0526
Ana Lastoviria <ana.lastoviria@zendesk.com>
2023-02-17 17:19:07 +1100

    fix broken standup posting after retro tuesdays

--- src/helper/validator.ts
+++ src/helper/validator.ts
@@ -24,40 +24,53 @@ export const validateEventType = (req: Request, res: Response, next: NextFunctio
 
 export const isThisRetroDay = async (req: Request, res: Response, next: NextFunction) => {
   const { event } = req.params;
-  const today = new Date();
   let tuesdayCount = await readTuesdayCountFile();
+  const today = new Date();
 
-  // 1 || 2 is to account for both UTC and AEST
-  if (event == Event.standup as string) {
-    res.send("Posting standup lead to Slack")
+  // Any other day except Tuesday - post standup
+  if ((event == Event.standup as string) && (today.getDay() !== 1)) {
     next();
-  } else if (tuesdayCount.count) {
-    console.log("Tuesday count before: " + JSON.stringify(tuesdayCount))
-    console.log("Tuesday count count: " + tuesdayCount.count)
+  }
 
-    const newTuesdayCount = { "count": 0 };
-    try {
-      await writeFile(newTuesdayCount, 'tuesday-count');
-      console.log("These are contents of the file now")
-      console.log(JSON.stringify(await readTuesdayCountFile()))
-      next();
-    } catch (e) {
-      res.send("Tuesday count file didn't write")
-      console.log(e)
+  // Tuesdays (Monday in UTC)
+  if (today.getDay() == 1) {
+    // regular Tuesday - post standup, no post retro
+    if (!tuesdayCount.count) {
+      if (event == Event.standup as string) {
+        console.log("Non-retro Tuesday, posting Standup lead")
+        next();
+      } else if (event == Event.retro as string) {
+        // change Tuesday count to 1 to post next week
+        const newTuesdayCount = { "count": 1 };
+        try {
+          // write it into file
+          await writeFile(newTuesdayCount, 'tuesday-count')
+          console.log("These are contents of the file now")
+          console.log(JSON.stringify(await readTuesdayCountFile()))
+          // don't post, just send response
+          res.send("Not posting to Slack, because it's not sprint review/retro Tuesday")
+        } catch (e) {
+          res.send("Tuesday count file didn't write")
+          console.log(e)
+        }
+      }
     }
-  } else if (!tuesdayCount.count) {
-    console.log("Tuesday count before: " + JSON.stringify(tuesdayCount))
-    console.log("Tuesday count count: " + tuesdayCount.count)
 
-    const newTuesdayCount = { "count": 1 };
-    try {
-      await writeFile(newTuesdayCount, 'tuesday-count')
-      console.log("These are contents of the file now")
-      console.log(JSON.stringify(await readTuesdayCountFile()))
-      res.send("Not posting to Slack, because it's not sprint review/retro Tuesday")
-    } catch (e) {
-      res.send("Tuesday count file didn't write")
-      console.log(e)
+    // retro Tuesday - post retro, no post standup
+    if (tuesdayCount.count) {
+      // change Tuesday count to 0
+      const newTuesdayCount = { "count": 0 };
+      // write it in file
+      try {
+        await writeFile(newTuesdayCount, 'tuesday-count');
+        console.log("These are contents of the Count file now")
+        console.log(JSON.stringify(await readTuesdayCountFile()))
+        // Post to Slack
+        next();
+      } catch (e) {
+        res.send("Tuesday count file didn't write")
+        console.log(e)
+      }
     }
   }
 }
